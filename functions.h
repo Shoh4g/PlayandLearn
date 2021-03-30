5cec0117b0a239fafc9c92d28004a59a34cbbf5a
Masood Ahmed <mangimasood2000@gmail.com>
2021-03-31 00:04:27 +0800

    Added a functions' header file

---
+++ functions.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void introduction ();
+
+void print_line_pattern( char character_to_print, int times );
+
+void diamond( char diamondCharacter, int m, int r);
+
+void typewriter( string str );
+
+#endif
+
+
+
+
