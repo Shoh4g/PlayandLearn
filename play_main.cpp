3def4d6fc5090201ab98b220920e5bea87fad6c7
Masood Ahmed <mangimasood2000@gmail.com>
2021-03-31 00:05:44 +0800

    Added the main c++ file for the game

---
+++ play_main.cpp
@@ -0,0 +1,18 @@
+// The main file of the Program/Project
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <ctime>
+#include <unistd.h> //As you are writing for windows, change it into <windows.h>
+#include <string>
+
+#include "functions.h"   //creating Header files
+
+using namespace std;
+
+
+int main()
+{
+    introduction();
+    return 0;
+}
