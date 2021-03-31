626a4f876aa1bde4115bdcfe9969c221ff41b147
Ahmed Masood <h3581212@academy11.cs.hku.hk>
2021-03-31 10:22:29 +0800

    Changed header files

--- play_main.cpp
+++ play_main.cpp
@@ -3,7 +3,13 @@
 #include <iomanip>
 #include <time.h>
 #include <ctime>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h> //As you are writing for windows, change it into <windows.h>
+#endif
+
 #include <string>
 
 #include "functions.h"   //creating Header files
