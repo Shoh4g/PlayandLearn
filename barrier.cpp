a65f794146cce76707149ca31d4c144d549ff614
Masood Ahmed <mangimasood2000@gmail.com>
2021-04-05 13:49:32 +0800

    created a barrier.cpp file

---
+++ barrier.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "barrier.h"
+#include "functions.h"
+
+using namespace std;
+
+
+//This function acts as a barrier between different parts of the game allowing easy readibility
+void barrier()
+{
+    cout << endl;
+    string line = "Please press Enter to continue.....\n";
+    typewriter(line);
+    cin.get();
+    system("clear");
+}
