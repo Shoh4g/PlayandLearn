4a6f22b33af47b78febe04f598b1e142ef91983b
Shoh4g <80630421+Shoh4g@users.noreply.github.com>
2021-04-25 16:42:54 +0800

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -59,18 +59,19 @@ bool random_guess()
     
     while (score==true)
     {
-        if (guess==1 && guess ==2 && guess==3 && guess ==4 && guess==5 && guess ==6)
+        if (guess==1 && guess ==2 && guess==3 && guess ==4 && guess==5 && guess ==6 && isdigit(guess)==true)
         {
             score=false;
         }
         
         cout<< "Invalid input! The number entered should be between 1 to 6.\n";
         cin >> guess;
-        if (isdigit(guess)==false)
-        {
-            cout<< "Invalid input! The number entered should be between 1 to 6.\n";
-            cin >> guess;    
-        }
+        
+        //if (isdigit(guess)==false)
+        //{
+            //cout<< "Invalid input! The number entered should be between 1 to 6.\n";
+            //cin >> guess;    
+        //}
             
     }
 
