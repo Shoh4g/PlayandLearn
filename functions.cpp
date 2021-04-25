c1ba5fba9db781ac964dae654d66195ee3c17459
Shoh4g <80630421+Shoh4g@users.noreply.github.com>
2021-04-25 15:51:19 +0800

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -177,7 +177,7 @@ string introduction()
     sleep(1);
     print_line_pattern( '_', 80 );
     sleep( 1 );
-    typewriter( line_1, 75000 );
+    
     print_line_pattern( '_', 80 );
     sleep( 1 );
 	
@@ -187,7 +187,7 @@ string introduction()
     
     while ( option!="y" && option!="Y" && option!="N" && option !="n")
     {
-	    cout << "Invalid answer! Please enter (Y/N).\n";
+	    cout << "Invalid answer! Please enter (Y/N)"<<endl;
 	    cin.ignore();
 	    cin >> option;	    
     }
@@ -195,7 +195,7 @@ string introduction()
    if (option=="Y" || option=="y")
    { 
  	
-	
+    typewriter( line_1, 75000 );
     typewriter( description_1, 75000 );
     typewriter( description_2, 75000 );
     typewriter( description_3, 75000 );
